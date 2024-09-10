44cc470f99b46e66be994bbda2bff32898f28c9b
ichsansandy <ichsan2668@gmail.com>
2024-09-10 22:06:00 +0700

    feat: add validation for transaction_type

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,15 +1,15 @@
 class Transaction < ApplicationRecord
   self.inheritance_column = :type
+  TRANSACTION_TYPES = %w[credit debit].freeze
 
   belongs_to :source_wallet, class_name: 'Wallet', optional: true
   belongs_to :target_wallet, class_name: 'Wallet', optional: true
 
   validates :amount, presence: true, numericality: { greater_than: 0 }
-  validates :transaction_type, presence: true
+  validates :transaction_type, presence: true, inclusion: { in: TRANSACTION_TYPES }
 
   validate :validate_transaction
 
-  TRANSACTION_TYPES = %w[credit debit].freeze
 
   private
 
