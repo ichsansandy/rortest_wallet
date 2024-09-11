1140f3e5af85a68d6a5a42b44b7a9fafe8cf4af5
ichsansandy <ichsan2668@gmail.com>
2024-09-11 08:40:19 +0700

    feat: add rswag

---
+++ spec/requests/api/v1/transactions_spec.rb
@@ -0,0 +1,55 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/v1/transactions', type: :request do
+
+  path '/api/v1/transactions/credit' do
+
+    post('credit transaction') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/transactions/debit' do
+
+    post('debit transaction') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/transactions/transfer' do
+
+    post('transfer transaction') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
